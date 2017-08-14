2758c866567c68edd13e71ee9ff2389885d57d93
Christian Kniep <christian@qnib.org>
2017-08-14 22:17:02 +0200

    add interface and structs

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,5 +1,10 @@
 package qtypes_plugin
 
+import (
+	"github.com/zpatrick/go-config"
+	"github.com/qframe/types/qchannel"
+)
+
 type Plugin struct {
 	Base
 	MyID		int
@@ -10,7 +15,13 @@ type Plugin struct {
 }
 
 
-func NewNamedPlugin(b Base, typ, pkg, name, version string) *Plugin {
+func NewNamedPlugin(qChan qtypes_qchannel.QChan, cfg *config.Config, typ, pkg, name, version string) *Plugin {
+	b := NewBase(qChan, cfg)
+	return NewBasePlugin(b, typ, pkg, name, version)
+}
+
+
+func NewBasePlugin(b Base, typ, pkg, name, version string) *Plugin {
 	p := &Plugin{
 		Base: b,
 		Typ:   		typ,
@@ -19,4 +30,12 @@ func NewNamedPlugin(b Base, typ, pkg, name, version string) *Plugin {
 		Name: 		name,
 	}
 	return p
+}
+
+func (p *Plugin) GetInfo() (typ,pkg,name string) {
+	return p.Typ, p.Pkg, p.Name
+}
+
+func (p *Plugin) GetLogOnlyPlugs() []string {
+	return p.LogOnlyPlugs
 }
